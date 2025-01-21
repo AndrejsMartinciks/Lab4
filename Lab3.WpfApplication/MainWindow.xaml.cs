b42ed65386e28fafb7e7d78d60cf272d0f5121c4
AndrejsMartinciks <Andrejs.Martinciks@edu.rtu.lv>
2025-01-21 19:53:38 +0200

    lab3

---
+++ Lab3.WpfApplication/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Lab3.WpfApplication.ViewModels;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab3.WpfApplication
+{
+    public partial class Window2 : Window
+    {
+        private MainWindowViewModel _viewModel;
+
+        public Window2()
+        {
+            InitializeComponent();
+            InitModel();
+        }
+
+        private void InitModel()
+        {
+            _viewModel = new MainWindowViewModel();
+            _viewModel.Load();
+            this.DataContext = _viewModel;
+        }
+    }
+}
---
+++ Lab3.WpfApplication/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,83 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Lab2.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Lab3.WpfApplication.ViewModels
+{
+    public class MainWindowViewModel : ObservableObject, INotifyPropertyChanged
+    {
+        private AuthorDbContext _db;
+        private Author[] _authors;
+        private List<Book> _books = new List<Book>();
+        private Author _selectedAuthor;
+
+        public MainWindowViewModel()
+        {
+            _db = new AuthorDbContext();
+            SelectAuthorCommand = new RelayCommand(LoadBooks);
+        }
+
+        public Author[] Authors
+        {
+            get => _authors;
+            private set
+            {
+                _authors = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public List<Book> Books
+        {
+            get => _books;
+            set
+            {
+                _books = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Author SelectedAuthor
+        {
+            get => _selectedAuthor;
+            set
+            {
+                _selectedAuthor = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand SelectAuthorCommand { get; }
+
+        public void Load()
+        {
+            Authors = _db.Authors.ToArray();
+        }
+
+        public void LoadBooks()
+        {
+            if (SelectedAuthor == null)
+            {
+                Books = new List<Book>();
+                return;
+            }
+
+            Books = _db.Books.Where(b => b.Author.Id == SelectedAuthor.Id).ToList();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
